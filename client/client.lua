4605e11f23f584c3b66fb70ecd4396c473dccec6
Alexandre Ribeiro <alexandrealen@hotmail.com>
2024-12-28 17:26:14 -0300

    update

--- client/client.lua
+++ client/client.lua
@@ -2,21 +2,21 @@
 
 local function getFile(port)
     print("Connecting")
-    local req = http.get("http://localhost:4546")
+    local req = http.get("http://localhost:" .. port)    
     if req == nil then
       print("Cannot connect")
       return
     end
     return req.readAll()
 end
- 
- 
+
+
 local function saveFile(file, filename)
     local f = fs.open(filename, "w")
     f.write(file)
     f.close()
 end
- 
-local file = getFile(8080)
-saveFile(file, "teste.lua")
+
+local file = getFile(4546)
+saveFile(file, "run.lua")
  
